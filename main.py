3d20f22e0c4dee3a335a59bb7b1665f7ab812009
Alex Bash <avbashlykov@gmail.com>
2015-11-15 12:33:32 +0300

    dal

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from blockcypher import get_transaction_details
 
 class ChainRegister:
 
-    def __init__(self, salt="hack4people", key="478e16e4aeee8e5aa9e5c9a1f6c978fe"):
+    def __init__(self, salt="hack4people", token="478e16e4aeee8e5aa9e5c9a1f6c978fe"):
         '''
         :param salt: secret word
         :param key: API key for blockcypher
         :return: hash of transaction
         '''
-        self.key = key
+        self.key = token
 
     def register_purchase(self, data):
         return embed_data(to_embed=data, api_key=self.key, data_is_hex=False)
